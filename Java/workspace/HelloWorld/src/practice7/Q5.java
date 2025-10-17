b90da1b3c77042f6ca3f6b5fd3006d7e643c656f
hyedonglim <hyedonglim@gmail.com>
2025-10-17 16:42:41 +0900

    2025-10-17 수업 내용 업로드

--- Java/workspace/HelloWorld/src/practice7/Q5.java
+++ Java/workspace/HelloWorld/src/practice7/Q5.java
@@ -1,10 +1,117 @@
 package practice7;
 
 class Phone{
+	private String maker;
+	private int price;
+	private String type;
+	
+	public Phone(String maker, int price, String type) {
+		this.maker = maker;
+		this.price = price;
+		this.type = type;
+	}
+	
+	public String getMaker() {
+		return maker;
+	}
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 	
 }
-class SmartPhone{
+class SmartPhone extends Phone{
+	// 멤버 변수
+	private String osType;
+	private String osVersion;
+	private int memory;
+	private boolean camera;
+	private boolean bluetooth;
+	//생성자
+	public SmartPhone(String maker, int price, String type, String osType, String osVersion, int memory, boolean camera,
+			boolean bluetooth) {
+		super(maker, price, type);
+		this.osType = osType;
+		this.osVersion = osVersion;
+		this.memory = memory;
+		this.camera = camera;
+		this.bluetooth = bluetooth;
+	}
+	//Getter, Setter
+	public String getOsType() {
+		return osType;
+	}
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+	public String getOsVersion() {
+		return osVersion;
+	}
+	public void setOsVersion(String osVersion) {
+		this.osVersion = osVersion;
+	}
+	public int getMemory() {
+		return memory;
+	}
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+	public boolean isCamera() {
+		return camera;
+	}
+//	카메라 지원 여부를 글자로 출력하는 메서드
+	public String getCamera() {
+		return camera ? "지원" : "미지원";
+	}
+	public void setCamera(boolean camera) {
+		this.camera = camera;
+	}
+	public boolean isBluetooth() {
+		return bluetooth;
+	}
+//	블루투스 지원 여부를 글자로 출력하는 메서드
+	public String getBluetooth() {
+		return bluetooth ? "지원" : "미지원";
+	}
+	public void setBluetooth(boolean bluetooth) {
+		this.bluetooth = bluetooth;
+	}
+	// 지원여부 출력 메서드
+	public String getBool(boolean bool) {
+		return bool ? "지원" : "미지원";
+	}
 	
+	@Override
+	public String toString() {
+//		제조사 : 삼성
+//		가격 : 1000000
+//		통신타입 : 2g
+//		운영체제 타입 : 안드로이드
+//		운영체제 버전 : 3.1.4
+//		내부 메모리 : 4gb
+//		카메라 장착 여부 : 지원
+//		블루투스 지원 여부 : 미지원
+		System.out.println("제조사 : " + getMaker());
+		System.out.println("가격 : " + getPrice());
+		System.out.println("통신타입 : " + getType());
+		System.out.println("운영체제 타입 : " + getOsType());
+		System.out.println("운영체제 버전 : " + getOsVersion());
+		System.out.println("내부 메모리 : "  + getMemory());
+		System.out.println("카메라 장착 여부 : " + getCamera());
+		System.out.println("블루투스 지원 여부 : "+ getBool(bluetooth));
+		return getMaker() + " " + getOsType();
+	}
 }
 
 public class Q5 {
@@ -18,7 +125,7 @@ public class Q5 {
 //		정자를 포함하여서 각각의 클래스를 작성한다. 이들 클래스들의 객체를 만들고 각 객체
 //		의 모든 정보를 출력하는 테스트 클래스를 작성하라. 
 		SmartPhone sp1 = new SmartPhone("삼성",1000000,"2g","안드로이드"
-				,"3.1.4","4gb",true, false);
+				,"3.1.4",4,true, false);
 		sp1.toString();
 //		=================출력결과===================
 //		제조사 : 삼성 
